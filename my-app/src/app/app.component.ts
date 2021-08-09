7f7f89fc64e6816eca8a17e1b1bf8de7e4879f08
msraosurya <ptlmnj89@gmail.com>
2021-08-09 08:15:14 +0530

    Angular parallax

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, NgModule } from '@angular/core';
 
-// comp start
 @Component({
   selector: 'app-start',
-  template: '<h1>hi Angular...!</h1>',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class MyComponent {}
-// comp end
