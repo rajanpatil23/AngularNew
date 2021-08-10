d56945333b02e7ea4c32f6cdfa8f02da731918a6
msraosurya <ptlmnj89@gmail.com>
2021-08-10 08:57:49 +0530

    ngclass ngstyle

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -9,9 +9,30 @@ export class MyComponent {
   title = 'Hello Angular...!';
 
   products = [
-    { prodId: 1001, prodName: 'Mobile', inStock: 100, country: 'India' },
-    { prodId: 1002, prodName: 'Laptop', inStock: 10, country: 'Germany' },
-    { prodId: 1003, prodName: 'Books', inStock: 0, country: '' },
+    {
+      prodId: 1001,
+      prodName: 'Mobile',
+      inStock: 100,
+      country: 'India',
+      isVeg: true,
+      showClass: false,
+    },
+    {
+      prodId: 1002,
+      prodName: 'Laptop',
+      inStock: 10,
+      country: 'Germany',
+      isVeg: false,
+      showClass: true,
+    },
+    {
+      prodId: 1003,
+      prodName: 'Books',
+      inStock: 0,
+      country: '',
+      isVeg: true,
+      showClass: true,
+    },
   ];
 }
 
