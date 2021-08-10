91e666c41c864c6fe264fa180bfcb072205b59bb
msraosurya <ptlmnj89@gmail.com>
2021-08-10 08:28:52 +0530

    ng switch, ng if

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -9,9 +9,9 @@ export class MyComponent {
   title = 'Hello Angular...!';
 
   products = [
-    { prodId: 1001, prodName: 'Mobile' },
-    { prodId: 1002, prodName: 'Laptop' },
-    { prodId: 1003, prodName: 'Books' },
+    { prodId: 1001, prodName: 'Mobile', inStock: 100, country: 'India' },
+    { prodId: 1002, prodName: 'Laptop', inStock: 10, country: 'Germany' },
+    { prodId: 1003, prodName: 'Books', inStock: 0, country: '' },
   ];
 }
 
